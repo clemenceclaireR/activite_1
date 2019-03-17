870f00b8ad91cb45c325d43d8957c17d832a1eb8
clemenceclaireR <clemence.robinletellier@gmail.com>
2019-03-17 21:31:49 +0100

    Ajout des scripts aire_cercle et aire_triangle

---
+++ aire_cercle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: <UTF-8> -*-
+
+from math import pi
+
+def surfCercle(R):
+    return pi * R ** 2
+
+print(surfCercle(2.5))
---
+++ aire_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: <UTF-8> -*-
+
+from math import *
+
+a = input("Veuillez entrer le cote a :")
+b = input("Veuillez entrer le cote b :")
+c = input("Veuillez entrer le cote c :")
+
+cote_a = (float(a))
+cote_b = (float(b))
+cote_c = (float(c))
+
+demi_perimetre = (cote_a + cote_b + cote_c) / 2 
+aire = sqrt(demi_perimetre*(demi_perimetre-cote_a)*(demi_perimetre-cote_b*(demi_perimetre-cote_c)))
+
+print("Longueur des cotes =", cote_a, cote_b, cote_c)
+print("Perimetre =", 2*demi_perimetre, "Aire =", aire)
